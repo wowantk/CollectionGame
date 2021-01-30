2d4e1490362c04fb5c80facfe943504148780b25
Volodymyr Drobot <vova.drobot@icloud.com>
2021-01-30 13:12:39 +0200

    //add dismiss keyboard

--- CollectionGame/SettingsController.swift
+++ CollectionGame/SettingsController.swift
@@ -24,8 +24,13 @@ class SettingsController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     
@@ -42,6 +47,7 @@ class SettingsController: UIViewController {
     
     @IBAction func backButton(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+        
     }
     
     
